f42332f07eac7e938ba23babee6063d627dfcc9e
ajayk <83286361+ajuajay11@users.noreply.github.com>
2025-11-29 14:34:05 +0400

    removed unwanter rendering block

--- models/otpManager.ts
+++ models/otpManager.ts
@@ -9,7 +9,7 @@ export interface IotpManager extends Document {
 const OtpSchema: Schema<IotpManager> = new Schema({
   otp: { type: String, required: true, unique: true },
   email: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now, expires: 300 } // expires in 300s = 5 min
+  createdAt: { type: Date, default: Date.now, expires: 300 } // expires in 300s = 5 min
 
 });
 
